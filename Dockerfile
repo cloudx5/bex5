36c5354111d05631c3561680fb04bea24c22d107
663534597 <qc_chen@qq.com>
2016-03-23 15:30:15 +0800

    修改文件： Dockerfile BeX5 启动脚本 新增文件： WEB服务器JVM参数配置文件 WEB服务器JNDI数据源配置文件
    Signed-off-by: 663534597 <qc_chen@qq.com>

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,12 @@ ADD bex5.tar.gz /usr/local/
 
 COPY bex5/startup.sh /usr/local/startup.sh
 COPY bex5/mysql/bin/startup.sh /usr/local/mysql/bin/startup.sh
+COPY bex5/tomcat/bin/catalina.sh /usr/local/apache-tomcat/bin/catalina.sh
+COPY bex5/tomcat/config/context.xml /usr/local/apache-tomcat/conf/context.xml
 
 RUN yum install -y libaio
 RUN useradd mysql
 
-CMD /usr/local/startup.sh
 EXPOSE 8080
 
+CMD /usr/local/startup.sh
--- bex5/startup.sh
+++ bex5/startup.sh
@@ -3,13 +3,14 @@
 cd `dirname $0`
 HOME=$PWD
 
-echo "当前 HOME 目录：" $HOME
+echo "当前 HOME 目录：$HOME"
 
 echo "正在更新 ‘model’"
 rm -rf $HOME/model/*
 mv -f /mnt/mesos/sandbox/model/* $HOME/model
 
-echo `ll model`
+echo "`ll model`"
+echo "model 更新完毕..."
 echo ""
 
 cd $HOME/mysql/bin
@@ -19,17 +20,26 @@ echo ""
 
 sleep 10
 
-echo "正在初始化 SQL 脚本"
-for file in `ls /mnt/mesos/sandbox/sql`; do
-  if [ -f /mnt/mesos/sandbox/sql/$file ]; then
-    ./mysql --default-character-set=gbk -uroot -px5 x5sys < /mnt/mesos/sandbox/sql/$file
-    echo $file " 初始化完毕..."
-  fi
+echo "开始初始化 SQL 脚本"
+
+mv -f /mnt/mesos/sandbox/sql $HOME
+echo "`ll $HOME/sql`"
+echo ""
+
+if [ -d $HOME/sql ]; then
+for II in `ls $HOME/sql`; do
+  ./mysql -uroot -px5 bex5 < $HOME/sql/$II
+  echo "$II 初始化完成..."
 done
+fi
 echo "SQL 脚本初始化完毕..."
 echo ""
 
+echo "开始启动 Apache-tomcat 服务"
+echo "正在配置服务启动参数..."
+tmpstr="<version>`date +%d%M%S`</version>"
+sed -i "s#<version>.*</version>#$tmpstr#g" $HOME/conf/server.xml
+echo "正在启动 WEB 服务..."
 cd $HOME/apache-tomcat/bin
 ./startup.sh
-echo "Tomcat 服务启动完毕..."
 
